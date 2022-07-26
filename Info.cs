a524f9d66ba225348ae0a91e7abb9bbe9145d3aa
Equipo <Equipo@DESKTOP-3SLF09A>
2022-07-26 09:02:03 -0500

    Agregar archivos de proyecto.

---
+++ Info.cs
@@ -0,0 +1,29 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace itsco2
+{
+    
+    [Activity(Label = "Info", MainLauncher = false)]
+    public class Info : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+
+
+            SetContentView(Resource.Layout.Info);
+
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+        }
+    }
+}
---
+++ MainActivity.cs
@@ -0,0 +1,46 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace itsco2
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+
+            SetContentView(Resource.Layout.activity_main);
+
+            var btnInfo = FindViewById<Button>(Resource.Id.btnInfo);
+
+            btnInfo.Click += delegate
+            {
+                Intent itn = new Intent(this, typeof(Info));
+                StartActivity(itn);
+
+
+            };
+
+
+
+
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
